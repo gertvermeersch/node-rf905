0c1048859d90fdfb163064704e91be86b000dcf4
Gert Vermeersch <vermeersch.gert@gmail.com>
2016-07-05 23:53:41 +0200

    Fixed sending

--- nrf905.js
+++ nrf905.js
@@ -8,8 +8,8 @@ var sleep = require('sleep');
   AM - 22
   CD - 27
   PWR - 17
-  CE - 18
-  TXEN - 14
+  TRX_CE - 18
+  TX_EN - 14
 */
 
 /** nrf905 instruction set */
@@ -26,12 +26,12 @@ var DR = 4;
 var AM = 22;
 var CD = 27;
 var PWR = 17;
-var CE = 18;
-var TXEN = 14;
+var TRX_CE = 18;
+var TX_EN = 14;
 
 var Nrf905 = function(options) {
 
-  this.handlers = [];
+    this.handlers = [];
 };
 
 Nrf905.prototype.init = function() {
@@ -42,23 +42,32 @@ Nrf905.prototype.init = function() {
     });
 
     rpio.spiBegin();
-    rpio.spiChipSelect(0); //use CE0 as CE pin (active low)
+    rpio.spiChipSelect(0); //use TRX_CE0 as TRX_CE pin (active low)
     rpio.spiSetClockDivider(28); //approx. 9.25 Mhz, just below 10Mhz
     rpio.spiSetDataMode(0);
 
     rpio.open(PWR, rpio.OUTPUT);
     rpio.open(CSN, rpio.OUTPUT);
-    rpio.open(TXEN, rpio.OUTPUT);
-    rpio.open(CE, rpio.OUTPUT);
+    rpio.open(TX_EN, rpio.OUTPUT);
+    rpio.open(TRX_CE, rpio.OUTPUT);
+    rpio.open(AM, rpio.INPUT);
+    rpio.open(DR, rpio.INPUT);
+    rpio.open(CD, rpio.INPUT);
+
+
+    setInterval(function() {
+        console.log("AM: " + rpio.read(AM) + " DR: " + rpio.read(DR) + " CD: " + rpio.read(CD));
+    }, 5000);
+
     var configArray = [0xCE, 0x0D, 0x44, 0x20, 0x20, 0x00, 0x00, 0x00, 0x00, 0x58];
 
-    if (this.address === undefined)  {
-      this.address = [0x00, 0x00, 0x00, 0x00];
+    if (this.address === undefined) {
+        this.address = [0x00, 0x00, 0x00, 0x00];
     } else {
-      configArray[5] = this.address[0];
-      configArray[6] = this.address[1];
-      configArray[7] = this.address[2];
-      configArray[8] = this.address[3];
+        configArray[5] = this.address[0];
+        configArray[6] = this.address[1];
+        configArray[7] = this.address[2];
+        configArray[8] = this.address[3];
     }
 
     this.writeConfig(0, configArray);
@@ -66,117 +75,135 @@ Nrf905.prototype.init = function() {
 };
 
 Nrf905.prototype.writeConfig = function(startByte, configArray) {
-  rpio.write(PWR, 0);
-  rpio.write(CSN, 1);
+    rpio.write(PWR, 0);
+    rpio.write(CSN, 1);
 
-  //write config buffer
-  var cmd = new Buffer([WC || (0x0F && startByte)]);
-  var payloadBuffer = new Buffer(configArray);
-  rpio.write(CSN, 0);
-  rpio.spiWrite(cmd, 1);
-  rpio.spiWrite(payloadBuffer, payloadBuffer.length);
-  rpio.write(CSN, 1);
+    //write config buffer
+    var cmd = new Buffer([WC || (0x0F && startByte)]);
+    var payloadBuffer = new Buffer(configArray);
+    rpio.write(CSN, 0);
+    rpio.spiWrite(cmd, 1);
+    rpio.spiWrite(payloadBuffer, payloadBuffer.length);
+    rpio.write(CSN, 1);
+    rpio.write(PWR, 1); //power up
 };
 
 Nrf905.prototype.readConfig = function() {
-  rpio.write(CSN, 0);
-  rpio.spiWrite(new Buffer([RC]), 1);
-  var tx = new Buffer([0x00]);
-  var rx = new Buffer(1);
-  var payload = "";
-  for (var i = 0; i < 10; i++) {
-      rpio.spiTransfer(tx, rx, 1);
-      payload = payload + rx.readUInt8(0).toString(16) + " ";
-  }
-  rpio.write(CSN, 1);
-  return payload;
+    rpio.write(CSN, 0);
+    rpio.spiWrite(new Buffer([RC]), 1);
+    var tx = new Buffer([0x00]);
+    var rx = new Buffer(1);
+    var payload = "";
+    for (var i = 0; i < 10; i++) {
+        rpio.spiTransfer(tx, rx, 1);
+        payload = payload + rx.readUInt8(0).toString(16) + " ";
+    }
+    rpio.write(CSN, 1);
+    return payload;
 };
 
 Nrf905.prototype.setAddress = function(address) {
-  this.address = address.slice(0, 4);
-  var addressBuf = new Buffer(this.address);
-  this.writeConfig(5, addressBuf);
+    this.address = address.slice(0, 4);
+    var addressBuf = new Buffer(this.address);
+    this.writeConfig(5, addressBuf);
 };
 
 Nrf905.prototype.startReceiveMode = function() {
-  rpio.write(TXEN, 0);
-  rpio.write(CE, 1);
-  rpio.poll(DR, function() {
-    this.receivePacket();
-  }, rpio.POLL_HIGH);
+    var self = this;
+    rpio.write(TX_EN, 0);
+    rpio.write(PWR, 1);
+    rpio.write(TRX_CE, 1);
+    sleep.usleep(1); //wait for mode change
+    rpio.poll(AM, function() {
+        self.receivePacket();
+    }, rpio.POLL_HIGH);
 };
 
 Nrf905.prototype.stopReceiveMode = function() {
-  rpio.poll(DR, null);
+    rpio.poll(DR, null);
 };
 
 Nrf905.prototype.receivePacket = function() {
-  if (rpio.read(AM) === 1) { //address match
-    rpio.write(CE, 0);
-    rpio.write(CSN,0);
-    rpio.spiWrite(new Buffer([RRP]), 1);
 
+    console.log("Receive!");
+    while (rpio.read(DR) === 0); //wait for data ready high
+    rpio.write(TRX_CE, 0);
+    rpio.write(CSN, 0);
+    sleep.usleep(1);
+    rpio.spiWrite(new Buffer([RRP]), 1);
+    sleep.usleep(1);
     var tx = new Buffer([0x00]);
     var rx = new Buffer(1);
     var payload = "";
     for (var i = 0; i < 32; i++) {
         rpio.spiTransfer(tx, rx, 1);
+        sleep.usleep(1);
         payload = payload + rx.readUInt8(0).toString();
     }
+    console.log("payload:" + payload.toString());
     rpio.write(CSN, 1);
-    for(var j; i < this.handlers.length; j++) {
-      this.handlers[j](null, payload);
+    sleep.usleep(1);
+    rpio.write(TRX_CE, 1);
+    sleep.usleep(1);
+
+    for (var j; j < this.handlers.length; j++) {
+        this.handlers[j](null, payload);
     }
-  }
+
+
+
 };
 
 Nrf905.prototype.sendPacket = function(address, payloadString) {
-  var buffer = new Buffer(payloadString);
-  console.log(buffer.toJSON());
+    var buffer = new Buffer(payloadString);
+    //console.log(buffer.toJSON());
 
-  //set transmit mode
-  rpio.write(CE, 0);
-  rpio.write(TXEN, 1);
+    //set transmit mode
+    rpio.write(PWR, 1);
+    rpio.write(TRX_CE, 0);
+    rpio.write(TX_EN, 1);
+    sleep.usleep(1);
+    //write packet data
+    rpio.write(CSN, 0);
+    rpio.spiWrite(new Buffer([WTP]), 1);
+    rpio.spiWrite(buffer, buffer.length);
+    rpio.write(CSN, 1);
 
-  //write packet data
-  rpio.write(CSN, 0);
-  rpio.spiWrite(new Buffer([WTP]), 1);
-  rpio.spiWrite(buffer, buffer.length);
-  rpio.write(CSN, 1);
+    //write address
+    rpio.write(CSN, 0);
+    rpio.spiWrite(new Buffer([WTA]), 1);
+    rpio.spiWrite(new Buffer(address), 4);
+    rpio.write(CSN, 1);
 
-  //write address
-  rpio.write(CSN, 0);
-  rpio.spiWrite(new Buffer([WTA]), 1);
-  rpio.spiWrite(new Buffer(address), 4);
-  rpio.write(CSN, 1);
+    //pulse TRX_CE to enable sending
+    rpio.write(TRX_CE, 1);
+    sleep.usleep(1);
+    rpio.write(TRX_CE, 0);
+    while (rpio.read(DR) === 0); //wait until sent
+    console.log("packet sent");
 
-  //pulse CE to enable sending
-  rpio.write(CE, 1);
-  sleep.usleep(1);
-  rpio.write(CE, 0);
-
-  rpio.write(TXEN, 1);
-  rpio.write(CE, 1);
+    rpio.write(TX_EN, 0);
+    rpio.write(TRX_CE, 1);
 };
 
 Nrf905.prototype.attachReceivedCallback = function(callback) {
-  this.handlers.push(callback);
+    this.handlers.push(callback);
 };
 
 Nrf905.prototype.detachReceivedCallback = function(callback) {
-  var index = this.handlers.indexOf(callback);
-  if (index > -1) {
-    this.handlers.splice(index, 1);
-  }
+    var index = this.handlers.indexOf(callback);
+    if (index > -1) {
+        this.handlers.splice(index, 1);
+    }
 };
 
 Nrf905.prototype.powerUp = function() {
-  rpio.write(PWR, 1);
-  sleep.usleep(3); // 3 sec wake up time
+    rpio.write(PWR, 1);
+    sleep.usleep(3); // 3 sec wake up time
 };
 
 Nrf905.prototype.powerDown = function() {
-  rpio.write(PWR, 0);
+    rpio.write(PWR, 0);
 };
 
 module.exports = Nrf905;
--- test.js
+++ test.js
@@ -3,13 +3,14 @@ var Nrf905 = require('./nrf905.js');
 var radio = new Nrf905();
 
 radio.init();
-radio.setAddress([0x66, 0x66, 0x66, 0x66]);
+radio.setAddress([0x41, 0x41, 0x41, 0x41]);
+//radio.setAddress([0x66, 0x66, 0x66, 0x66]);
 console.log(radio.readConfig());
 
 radio.attachReceivedCallback(function(err, payload) {
-  console.log(payload);
+  console.log("received: " + payload);
 });
 
-radio.startReceiveMode();
-
-radio.sendPacket([0x77, 0x77, 0x77, 0x77], "abcdefABCDEF");
+//radio.startReceiveMode();
+var i = 0;
+setInterval(function() {radio.sendPacket([0x66, 0x66, 0x66, 0x66], "AAAAthis is packet " + i++ + "            ");}, 1000);
